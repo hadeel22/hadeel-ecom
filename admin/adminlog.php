fe9bb8fe6d67f7149399604569cbed025b31199a
fffff <hadoly-11@hotmail.com>
2018-06-24 14:06:53 +0300

    initial commit

---
+++ admin/adminlog.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<!--
+Transit by TEMPLATED
+templated.co @templatedco
+Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Employer Register</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+<script src="js/jquery.min.js"></script>
+<script src="js/skel.min.js"></script>
+<script src="js/skel-layers.min.js"></script>
+<script src="js/init.js"></script>
+<noscript>
+<link rel="stylesheet" href="css/skel.css" />
+<link rel="stylesheet" href="css/style.css" />
+<link rel="stylesheet" href="css/style-xlarge.css" />
+</noscript>
+</head>
+
+<body>
+
+
+ 
+<!-- Main -->
+<!-- Header -->
+<header id="header">
+<h1><a></a></h1>
+<nav id="nav">
+<ul>
+<li><a href="adminlog.php">Hadeel E-commerce</a></li>
+
+
+
+            
+</ul>
+</nav>
+</header>
+<!-- Main -->
+<section id="main" class="wrapper">
+<div class="container">
+ 
+<header class="major">
+<h2>Log in</h2>
+</header>
+
+
+</html>
